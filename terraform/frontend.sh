5b16ee4d6e9661837e4bb1c38f41d098b37d3e10
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-10-05 10:25:21 -0700

    ALL

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -6,4 +6,5 @@ sudo dnf install ansible -y
 pip3.9 install botocore boto3
 #ansible-pull -i localhost, -U https://github.com/NarendraNReddy/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
 
-ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$3
+#ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$3
+ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf-jenkins.git main.yaml -e component=$component -e env=$environment -e appVersion=$3
